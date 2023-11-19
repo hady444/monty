05a4eb2aa6cf12b1305458fddb7bdb1f22cf4ab8
hady444 <hadyhassan2312@gmail.com.com>
2023-11-19 21:10:36 +0000

    c

--- rotr.c
+++ rotr.c
@@ -5,7 +5,7 @@
  */
 void my_rotr_f(stack_t **data_str, __attribute__((unused))unsigned int line_count)
 {
-	stack_t *traverser;
+	stack_t *traverser, *node;
 
         if (data_str == NULL || (*data_str) == NULL || (*data_str)->next == NULL)
 		return;
@@ -15,6 +15,7 @@ void my_rotr_f(stack_t **data_str, __attribute__((unused))unsigned int line_coun
 	traverser->next = *data_str;
 	traverser->prev->next = NULL;
 	traverser->prev = NULL;
-	(*data_str)->prev = traverser;
+	node = *data_str;
+	node->prev = traverser;
 	(*data_str) = traverser;
 }
