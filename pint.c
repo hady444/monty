be89c29fee221b08535d1b652d92ff9428925bc2
hady444 <hadyhassan2312@gmail.com.com>
2023-11-19 17:30:56 +0000

    c

--- pint.c
+++ pint.c
@@ -13,6 +13,7 @@ void my_pint_f(stack_t **data_str, unsigned int line_number)
 	else
 	{
 		printf("L%d: can't pint, stack empty\n", line_number);
+		free_structure(*data_str), free(cont.line), free(cont.my_file);
 		exit(EXIT_FAILURE);
 	}
 }
