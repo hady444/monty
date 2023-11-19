576134c1bebcb8b2c5a4ea1430d39ef8e3ba05a2
hady444 <hadyhassan2312@gmail.com.com>
2023-11-19 20:35:48 +0000

    c

--- rotr.c
+++ rotr.c
@@ -3,10 +3,9 @@
  * @data_str: poitoer to head of stack
  * @line_count: count of current read line
  */
-void my_rotr_f(stack_t **data_str, unsigned int line_count)
+void my_rotr_f(stack_t **data_str, __attribute__((unused))unsigned int line_count)
 {
 	stack_t *traverser;
-	(void) line_count;
 
         if (data_str && (*data_str) && (*data_str)->next)
         {
